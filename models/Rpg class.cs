0e8cb884d57a5b46a6caca78b9327c2edc176580
Aman Tiwari <atiwari@pizoneinfotech.com>
2022-09-09 16:28:02 +0530

    jhgg

---
+++ models/Rpg class.cs
@@ -0,0 +1,14 @@
+using System.Text.Json.Serialization;
+
+
+
+namespace dotnet_rpg.models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Rpgclass
+    {
+        knight = 1,
+        mage =2,
+        cleric  =3,
+    }
+}
